29677a4dcf456f71fe7826d0c8f9855bb9581c98
ovtsunova <isip_a.a.ovtsunova@mpt.ru>
2026-03-25 13:35:35 +0300

    clean proj

---
+++ src/main/java/com/example/appliance/ApplianceApplication.java
@@ -0,0 +1,13 @@
+package com.example.appliance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplianceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplianceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appliance/ApplianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appliance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
